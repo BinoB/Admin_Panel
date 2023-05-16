7b018ab6e94512882fbf7e6976c744ce4e3ab81f
BinoB <binob831@gmail.com>
2023-05-16 22:39:40 +0530

    file removed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/DashboardPage/Dashboard";
-import Colors from "./components/UtilitiesPage/ColorsPage/Colors";
-import Borders from "./components/UtilitiesPage/BordersPage/Borders";
-import Animations from "./components/UtilitiesPage/AnimationsPage/Animations";
-import Others from "./components/UtilitiesPage/OthersPage/Others";
 import Login from "./components/AddonPages/Login";
 import Register from "./components/AddonPages/Register";
 import ForgotPassword from "./components/AddonPages/ForgotPassword";
@@ -13,7 +9,7 @@ import BlankPage from "./components/AddonPages/BlankPage";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Product from "./components/ComponentsPage/ProductPage/Product";
-import User from "./components/ComponentsPage/CardsPage/User";
+import User from "./components/ComponentsPage/UsersPage/User";
 
 function App() {
   return (
@@ -24,10 +20,6 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/product" element={<Product />} />
         <Route path="/user" element={<User />} />
-        <Route path="/colors" element={<Colors />} />
-        <Route path="/borders" element={<Borders />} />
-        <Route path="/animations" element={<Animations />} />
-        <Route path="/others" element={<Others />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
